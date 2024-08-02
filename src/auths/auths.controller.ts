d2e004e400dac97bdc14e5985648f6c1c8d21a75
xiivth <xiivth@whoru.co>
2024-08-02 15:02:16 +0700

    update

--- src/auths/auths.controller.ts
+++ src/auths/auths.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { AuthsService } from './auths.service';
+import { RegisterDto } from './dto/register.dto';
 
-@Controller('auths')
-export class AuthsController {}
+@Controller({ path: 'auths', version: '1' })
+export class AuthsController {
+  constructor(private readonly authsService: AuthsService) {}
+
+  @Post('register')
+  @HttpCode(201)
+  async register(@Body() registerDto: RegisterDto) {
+    await this.authsService.register(registerDto);
+    return { meesge: `${registerDto.email} ลงทะเบียนสำเร็จ` };
+  }
+}
--- src/auths/auths.service.ts
+++ src/auths/auths.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { hash, genSalt } from 'bcrypt';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthsService {}
+export class AuthsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    try {
+      const salt = await genSalt(10);
+      const hashedPassword = await hash(registerDto.password, salt);
+      const newUser = await this.prismaService.user.create({
+        data: { email: registerDto.email, password: hashedPassword },
+      });
+      return newUser;
+    } catch (error) {
+      throw new HttpException(error, 500);
+    }
+  }
+
+  login() {}
+}
