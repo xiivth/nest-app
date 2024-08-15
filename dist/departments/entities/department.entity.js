cca794b38720d90abdc1a0d7374e8288f5e72e4d
xiivth <158536134+xiivth@users.noreply.github.com>
2024-08-15 19:12:28 +0700

    Delete dist directory

--- dist/departments/entities/department.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Department = void 0;
-class Department {
-}
-exports.Department = Department;
-//# sourceMappingURL=department.entity.js.map
